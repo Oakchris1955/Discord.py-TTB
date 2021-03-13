72538f628f3be3fdc5e35ad106b5a4f32873f574
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2021-03-13 18:13:59 +0200

    Add files via upload

---
+++ discord.py
@@ -0,0 +1,25 @@
+import time
+
+felcanje = True
+
+print("Welcome to self bot!")
+name = input("Input your username: ")
+name2 = input("Input your second username: ")
+
+while felcanje == True:
+
+    text = input(name + " Input: ")
+
+    from discord_webhook import DiscordWebhook
+
+    webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/819959647247138836/FGj_5c6VMj6B2dvrBWi6IOZn_aMSdUKbHO0fbdJf_ZA5tgvhbHt4QuMkufBcaF99m3Q9', content="**" + name + ": " + "**" + text)
+    response = webhook.execute()
+
+    time.sleep(0.5)
+
+"""   text2 = input(name2 + " Input: ")
+
+    from discord_webhook import DiscordWebhook
+
+    webhook = DiscordWebhook(url='webhooklink', content="**" + name2 + ": " + "**" + text2)
+    response = webhook.execute() """
