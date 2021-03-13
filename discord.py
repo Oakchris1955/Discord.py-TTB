f8a2edc0b57198134d4f302ddd5d2cfa5c98d9c8
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2021-03-13 18:26:53 +0200

    Update discord.py

--- discord.py
+++ discord.py
@@ -4,7 +4,7 @@ felcanje = True
 
 print("Welcome to self bot!")
 name = input("Input your username: ")
-name2 = input("Input your second username: ")
+#name2 = input("Input your second username: ")
 
 while felcanje == True:
 
@@ -15,11 +15,11 @@ while felcanje == True:
     webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/819959647247138836/FGj_5c6VMj6B2dvrBWi6IOZn_aMSdUKbHO0fbdJf_ZA5tgvhbHt4QuMkufBcaF99m3Q9', content="**" + name + ": " + "**" + text)
     response = webhook.execute()
 
-    time.sleep(0.5)
+'''    time.sleep(0.5)
 
-"""   text2 = input(name2 + " Input: ")
+    text2 = input(name2 + " Input: ")
 
     from discord_webhook import DiscordWebhook
 
     webhook = DiscordWebhook(url='webhooklink', content="**" + name2 + ": " + "**" + text2)
-    response = webhook.execute() """
+    response = webhook.execute() '''
