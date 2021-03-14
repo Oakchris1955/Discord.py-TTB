ae9a10771dd3e91414ab0a6ac92ad78cd30e175c
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2021-03-14 18:04:53 +0200

    Update view-messages.py

--- Final/view-messages.py
+++ Final/view-messages.py
@@ -6,4 +6,4 @@ client = discord.Client()
 async def on_message(message):
 	print('In server:', message.guild, ', by:', message.author, ', Message:', message.content)
 
-client.run("Enter your bot's token here")
+client.run("Enter your bot's token here") #change this string ⏪
