46ba356269793f7f962ed5cd21721e3f33d0fd71
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2021-03-20 15:16:13 +0200

    Add files via upload

---
+++ test.pyw
@@ -0,0 +1,5 @@
+import subprocess
+#token = input("Enter your bot's token: ")
+subprocess.Popen(['python', 'bot-choose-guild.py'], start_new_session=True)
+subprocess.Popen(['python', 'view-messages.py'], start_new_session=True)
+#print(pidl.pid)
---
+++ view-messages.py
@@ -0,0 +1,15 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print('Logged in (read)')
+
+@client.event
+async def on_message(message):
+	print('In server:', message.guild, ', by:', message.author, ', Message:', message.content)
+
+#client.run(sys.argv[1:][0])
+#print(sys.argv[1:][0])
+client.run('ODE2OTYxNTM1NDAyNDQyNzYy.YECkuA.BTguNm7bLFYHRXubiCFXvtTemb0')
